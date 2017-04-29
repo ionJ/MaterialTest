b2603f7ecb15c9074b11fdd54ba3052f86766161
ionJ <ion_J@outlook.com>
2017-04-29 12:23:14 +0800

    用 Design Support 库提供的 FloatingActionButton 控件来实现一个悬浮按钮，并且为添加一个简单的点击事件

--- app/src/main/java/com/example/android/materialtest/MainActivity.java
+++ app/src/main/java/com/example/android/materialtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.materialtest;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem item){
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FAB clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
