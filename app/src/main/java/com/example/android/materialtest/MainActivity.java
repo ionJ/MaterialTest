f46cccd493117cbb115a47c045279d109df03ee0
ionJ <ion_J@outlook.com>
2017-05-06 12:06:21 +0800

    利用 support-v4 提供的 SwipeRefreshLayout 实现下拉刷新功能
    在 RecyclerView 外面嵌套一层 SwipeRefreshLayout ，使其拥有下拉刷新功能。然后在 MainActivity
    中添加具体的刷新逻辑。

--- app/src/main/java/com/example/android/materialtest/MainActivity.java
+++ app/src/main/java/com/example/android/materialtest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private List<Fruit> fruitList = new ArrayList<>();
 
     private FruitAdapter adapter;
+
+    private SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +87,37 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
+
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.
+                OnRefreshListener(){
+            @Override
+            public void onRefresh(){
+                refreshFruits();
+            }
+        });
+    }
+
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
     }
 
     private void initFruits() {
@@ -115,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.settings:
-                Toast.makeText(this, "You clicked Stettings", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
                 break;
             default:
         }
