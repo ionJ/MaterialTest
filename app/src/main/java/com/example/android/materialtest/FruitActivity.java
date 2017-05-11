f063e66c6c0d0d8f240c398f0471c4dc5938c431
ionJ <ion_J@outlook.com>
2017-05-11 22:51:03 +0800

    借助 Material Design 提供的 CollapsingToolbarLayout 实现了一个可折叠式标题栏
    位于点击水果图片后新的界面。

---
+++ app/src/main/java/com/example/android/materialtest/FruitActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.materialtest;
+
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class FruitActivity extends AppCompatActivity {
+
+    public static final String FRUIT_NAME = "fruit_name";
+
+    public static final String FRUIT_IMAGE_ID = "fruit_image_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fruit);
+        Intent intent = getIntent();
+        String fruitName = intent.getStringExtra(FRUIT_NAME);
+        int fruitImageId = intent.getIntExtra(FRUIT_IMAGE_ID, 0);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout)
+                findViewById(R.id.collapsing_toolbar);
+        ImageView fruitImageView = (ImageView) findViewById(R.id.fruit_image_view);
+        TextView fruitContentText = (TextView) findViewById(R.id.fruit_content_text);
+        // 将 ToolBar 作为 ActionBar 显示
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        collapsingToolbar.setTitle(fruitName);
+        // 用 Glide 加载传入的水果图片，并设置到标题栏的 ImageView 上面
+        // Glide.with(Context).load("图片路径").thumbnail("缩略比例").into("View组件")
+        Glide.with(this).load(fruitImageId).into(fruitImageView);
+        String fruitContent = generateFruitContent(fruitName);
+        fruitContentText.setText(fruitContent);
+    }
+
+    // 将水果名循环拼接 500 次生成一个字符串，作为填充的内容详情，设置到 TextView 上
+    private String generateFruitContent(String fruitName) {
+        StringBuilder fruitContent = new StringBuilder();
+        for (int i = 0; i < 500; i++) {
+            fruitContent.append(fruitName);
+        }
+        return fruitContent.toString();
+    }
+
+    // 处理 HomeAsUp 按钮的点击事件，点击时关闭当前活动，返回上一个活动
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
